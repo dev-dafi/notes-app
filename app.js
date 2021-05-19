e6a6066c0acea2f6d48bff20aaacff948eb85881
Daniel Fischer <dev_dafi@icloud.com>
2021-05-19 11:03:20 +0200

    Install npm validator

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// NPM MODULES
+const validator = require("validator");
+
+// LOCAL MODULES
 const getNotes = require("./notes.js");
+
+
 const notes = getNotes();
 console.log(notes);
+
+console.log(validator.isEmail("email@example.com"));
