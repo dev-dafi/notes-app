c2f6272353df016aed44572a33eea7eae7679bea
Daniel Fischer <dev_dafi@icloud.com>
2021-05-25 09:15:13 +0200

    Create addNote function check for duplicates and load notes, if available

--- app.js
+++ app.js
@@ -1,12 +1,67 @@
 // NPM MODULES
 const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
 // LOCAL MODULES
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 
 
-// const notes = getNotes();
-// console.log(notes);
+// ADD COMMAND
 
-console.log(chalk.green.inverse("SUCCESS!"));
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+      // CLI Option --
+      title: {
+          describe: "Note title",
+          demandOption: true,
+          type: "string",
+      },
+      body: {
+          describe: "Note description",
+          demandOption: true,
+          type: "string",
+      }
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+
+// REMOVE COMMAND
+
+yargs.command({
+  command: "remove",
+  describe: "Removes a note",
+  handler: function () {
+    console.log("Removing note");
+  }
+});
+
+
+// LIST COMMAND
+
+yargs.command({
+  command: "list",
+  describe: "Lists all notes",
+  handler: function () {
+    console.log("List notes");
+  }
+});
+
+
+// READ COMMAND
+
+yargs.command({
+    command: "read",
+    describe: "Read note",
+    handler: function() {
+        console.log("Reading note");
+    }
+});
+
+// console.log(yargs.argv);
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,7 +1,72 @@
+const chalk = require("chalk");
+const fs = require("fs");
+
 const getNotes = function () {
     return "Your notes ... ";
 };
 
-module.exports = getNotes;
+
+/**
+ * Save note to filesystem.
+ * @param {String} title 
+ * @param {String} body 
+ */
+
+const addNote = function (title, body) {
+    const notes = loadNotes();
+
+    // Filter function gets called for EVERY entry in notes and stores duplicate in duplicateNotes
+    const duplicateNotes = notes.filter(function(note){
+        // true if title exists ==> found duplicate
+        return note.title === title;
+    })
+    
+    // Didn't find duplicate?
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title : title,
+            body : body,
+        });
+        saveNotes(notes);
+        console.log(chalk.green.inverse("New note added."));
+    } else {
+        console.log(chalk.red.inverse("Note title already taken."));
+    }
+}
+
+
+/**
+ * Load stored notes if available.
+ * @returns Stored notes
+ */
+
+const loadNotes = function (){
+
+    try {
+        // read file from filesystem
+        const dataBuffer = fs.readFileSync("notes.json");
+        const dataJSON = JSON.stringify(dataBuffer);
+        return JSON.parse(dataBuffer);
+    } catch(e) {
+        // notes file doesn't exist? return empty array
+        return [];
+    }
+}
+
+
+/**
+ * Save note into file
+ * @param {[notes]} notes 
+ */
+
+const saveNotes = function (notes){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync("notes.json", dataJSON);
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
 
 
